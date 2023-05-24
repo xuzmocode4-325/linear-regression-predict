b2ce80b88e70ab643ef9094f0838b956d714b6a7
Katlego Thobye <katlegothobye@yahoo.com>
2023-05-24 10:18:25 +0200

    updated model file

--- model.py
+++ model.py
@@ -58,7 +58,66 @@ def _preprocess_data(data):
     # ---------------------------------------------------------------
 
     # ----------- Replace this code with your own preprocessing steps --------
-    predict_vector = feature_vector_df[['Madrid_wind_speed','Bilbao_rain_1h','Valencia_wind_speed']]
+   # engineer existing features for test dataset
+    # 1. Isolating the target variable
+    target_variable = 'load_shortfall_3h'
+    # 2. Order the columns in alphabetical order
+    feature_vector_df = feature_vector_df.reindex(sorted(feature_vector_df.columns), axis=1)
+    # 3. Keep the "time" column in the first index position
+    feature_vector_df = feature_vector_df[['time'] + [col for col in feature_vector_df.columns if col != 'time']]
+    # 4. Convert all column titles to lowercase
+    feature_vector_df.columns = feature_vector_df.columns.str.lower()
+    # 5. Replacing null values in 'valencia_pressure' with the feature median value
+    valencia_pressure_median = feature_vector_df['valencia_pressure'].median()
+    feature_vector_df['valencia_pressure'] = feature_vector_df['valencia_pressure'].fillna(valencia_pressure_median)
+    # 6. converting categorical features to a numeric format
+    for col in feature_vector_df.columns:
+        if feature_vector_df[col].dtype == object and col != "time":
+            feature_vector_df[col] = feature_vector_df[col].str.extract(r'([0-9]+)')
+            feature_vector_df[col] = pd.to_numeric(feature_vector_df[col])
+    # 7. removing columns
+    cols_to_remove = []
+    for col in feature_vector_df.columns:
+        if "rain" in col or "snow" in col:
+            cols_to_remove.append(col)
+        if "temp" in col and "min" not in col:
+            cols_to_remove.append(col)
+        if "clouds" in col:
+            cols_to_remove.append(col)
+        if "weather" in col:
+            cols_to_remove.append(col)
+        if "deg" in col:
+            cols_to_remove.append(col)
+        if "humidity" in col:
+            cols_to_remove.append(col)
+    # 8. selecting features to keep
+    keep_cols = []
+    for col in feature_vector_df.columns:
+        if col not in cols_to_remove:
+            keep_cols.append(col)
+    feature_vector_df = feature_vector_df[keep_cols]
+    # 9. changing time colum from string type to datetime object and then to a delta time feature
+    feature_vector_df['time'] = pd.to_datetime(feature_vector_df['time'])
+    feature_vector_df['time_delta_hours'] = (feature_vector_df['time'] - feature_vector_df['time'].min()).dt.components['hours']
+    feature_vector_dict = feature_vector_df.drop("time", axis=1)
+    # 10. reordering the columns to place delta time first
+    feature_vector_df = feature_vector_df[['time_delta_hours'] + [col for col in feature_vector_df.columns if col != 'time_delta_hours']]
+    # 11. isolating feature columns 
+    features_cols = [col for col in feature_vector_df.columns != target_variable]
+    # 12. scaling features to prepare for the model
+    from sklearn.preprocessing import MinMaxScaler
+        # Create a MinMaxScaler object
+    scaler = MinMaxScaler()
+        # Fit the scaler to the data
+    y = feature_vector_df[target_variable]
+    X = feature_vector_df[features_cols]
+    scaler.fit(feature_vector_df[features_cols])
+    # Transform the data
+    scaled_data = scaler.transform(feature_vector_df[features_cols])
+    # Update the dataframe with the scaled data
+    feature_vector_df[features_cols] = scaled_data
+    # Display the scaled data
+    predict_vector = feature_vector_df.drop(target_variable, axis=1)
     # ------------------------------------------------------------------------
 
     return predict_vector
